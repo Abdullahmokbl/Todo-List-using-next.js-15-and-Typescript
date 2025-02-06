a29b53dcf12bfa15dc5861af9daf1649febe4076
abdullahmokbl <abdullahmokbl0@gmail.com>
2025-02-06 22:46:40 +0200

    metadata-favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Todo List',
+  description: 'keep it simple with todo list',
+};
 export default function RootLayout({
   children,
 }: Readonly<{
